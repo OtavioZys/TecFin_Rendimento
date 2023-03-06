d9c156a1ac526edb3a2bf9a96d78b0f7c25220c4
Otávio Zys <110419121+OtavioZys@users.noreply.github.com>
2023-03-06 13:46:55 -0400

    Tratamento de erros
    Caso haja erro de API loga em tabela e envia email com o erro

--- tecfin_getters.py
+++ tecfin_getters.py
@@ -1,12 +1,18 @@
-import json 
-import requests 
-from configs import TECFIN_USER, TECFIN_PASSWORD, TECFIN_DOMAIN, TECFIN_LOGIN_ENDPOINT, TECFIN_PRORROGACOES_ENDPOINT
+import json
+import requests
+from apps.tecfin.send_email import alerta_erro
+from integracoes_card.settings import TECFIN_USER, TECFIN_PASSWORD, TECFIN_DOMAIN, TECFIN_LOGIN_ENDPOINT, \
+    TECFIN_PRORROGACOES_ENDPOINT
+
 
 def tecfin_login():
+    """
+    FAZ LOGIN NA API TECFIN E RETORNA O ACCESS TOKEN
+    """
     url = f'{TECFIN_DOMAIN}{TECFIN_LOGIN_ENDPOINT}'
     payload = json.dumps({
         "login": TECFIN_USER,
-        "senha": TECFIN_PASSWORD 
+        "senha": TECFIN_PASSWORD
     })
     headers = {
         'Content-Type': 'application/json'
@@ -20,9 +26,16 @@ def tecfin_login():
         return access_token
     else:
         print(f'ERRO! API tecfin_login retornou erro HTTP {response.status_code}')
+        cod = response.status_code
+        erro = response.text
+        alerta_erro(cod, erro, "tecfin_login")
         return 0
 
+
 def get_prorrogacoes(data):
+    """
+    CONECTA NA API TECFIN E RETORNA AS PRORROGAÇÕES DA DATA FORNECIDA
+    """
     token = tecfin_login()
     url = f'{TECFIN_DOMAIN}{TECFIN_PRORROGACOES_ENDPOINT}?page=0&size=9000&order=DESC&dataInicial={data}&dataFinal={data}'
     payload = {}
@@ -33,7 +46,11 @@ def get_prorrogacoes(data):
 
     response = requests.request("GET", url, headers=headers, data=payload)
     if response.status_code == 200:
-        return response.json()
+        response_json = response.json()
+        return response_json
     else:
         print(f'ERRO! API get_prorrogacoes retornou erro HTTP {response.status_code}')
+        cod = response.status_code
+        erro = response.text
+        alerta_erro(cod, erro, "tecfin_prorrogacoes")
         return []
