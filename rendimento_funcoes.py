9c615a49551eed21635a877cd017c315d8a11e95
Otávio Zys <110419121+OtavioZys@users.noreply.github.com>
2023-05-05 11:11:29 -0400

    Update funções
    Tratamento e controle para evitar repetição de registros

--- rendimento_funcoes.py
+++ rendimento_funcoes.py
@@ -1,33 +1,49 @@
-import json
 import locale
-from rendimento_getters import get_extrato
-from datetime import datetime, timedelta, date 
+from datetime import datetime, timedelta, date
+from time import sleep
+
+from apps.extrato.models import Item
+from apps.extrato.getters import get_extrato
 
 locale.setlocale(locale.LC_ALL, '')
 
-def data_formatada():
-    ano = int(date.today().strftime('%Y'))
-    mes = int(date.today().strftime('%m'))
-    dia1 = int((date.today() - timedelta(days=1)).strftime('%d'))
-    dia2 = int((date.today() - timedelta(days=2)).strftime('%d'))
-    form23 = datetime(year=ano, month=mes, day=dia2, hour=23, minute=0, second=0)
-    form22 = datetime(year=ano, month=mes, day=dia1, hour=22, minute=59, second=59)
-    format = f'{form23} à {form22}'
 
-    return format
+def data_formatada_extrato():
+    data = int(date.today().strftime('%w'))
+    if data == 1:
+        ano = int(date.today().strftime('%Y'))
+        mes = int((date.today() - timedelta(days=4)).strftime('%m'))
+        dia1 = int((date.today() - timedelta(days=3)).strftime('%d'))
+        dia2 = int((date.today() - timedelta(days=4)).strftime('%d'))
+        form23 = datetime(year=ano, month=mes, day=dia2, hour=23, minute=0, second=0)
+        form22 = datetime(year=ano, month=mes, day=dia1, hour=22, minute=59, second=59)
+        sexta_format = f'{form23} à {form22}'
 
-def checa_dia(filtro):
+        return sexta_format
+    else:
+        ano = int(date.today().strftime('%Y'))
+        mes = int((date.today() - timedelta(days=2)).strftime('%m'))
+        dia1 = int((date.today() - timedelta(days=1)).strftime('%d'))
+        dia2 = int((date.today() - timedelta(days=2)).strftime('%d'))
+        form23 = datetime(year=ano, month=mes, day=dia2, hour=23, minute=0, second=0)
+        form22 = datetime(year=ano, month=mes, day=dia1, hour=22, minute=59, second=59)
+        ontem_format = f'{form23} à {form22}'
+
+        return ontem_format
+
+
+def checa_dia_extrato(filtro):
     data = date.today() - timedelta(days=filtro)
     lista_feriados = [
-            {'dia': 1, 'mes': 1, 'feriado': 'Ano-Novo'},
-            {'dia': 21, 'mes': 4, 'feriado': 'Tiradentes'},
-            {'dia': 1, 'mes': 5, 'feriado': 'Dia do Trabalhador'},
-            {'dia': 7, 'mes': 9, 'feriado': 'Declaração da Independência'},
-            {'dia': 12, 'mes': 10, 'feriado': 'Nossa Sra Aparecida'},
-            {'dia': 2, 'mes': 11, 'feriado': 'Finados'},
-            {'dia': 15, 'mes': 11, 'feriado': 'Proclamação da República'},
-            {'dia': 25, 'mes': 12, 'feriado': 'Natal'}
-        ]
+        {'dia': 1, 'mes': 1, 'feriado': 'Ano-Novo'},
+        {'dia': 21, 'mes': 4, 'feriado': 'Tiradentes'},
+        {'dia': 1, 'mes': 5, 'feriado': 'Dia do Trabalhador'},
+        {'dia': 7, 'mes': 9, 'feriado': 'Declaração da Independência'},
+        {'dia': 12, 'mes': 10, 'feriado': 'Nossa Sra Aparecida'},
+        {'dia': 2, 'mes': 11, 'feriado': 'Finados'},
+        {'dia': 15, 'mes': 11, 'feriado': 'Proclamação da República'},
+        {'dia': 25, 'mes': 12, 'feriado': 'Natal'}
+    ]
     is_feriado = bool
     dia = int(data.strftime('%d'))
     mes = int(data.strftime('%m'))
@@ -37,42 +53,172 @@ def checa_dia(filtro):
             is_feriado = True
             break
         else:
-            is_feriado = False 
-    
+            is_feriado = False
+
     return is_feriado
 
 def get_cobrancas_rendimento():
+    """
+    VERIFICA DIA, SE SEGUNDA FAZ BAIXA DO EXTRATO DE SEXTA, DO CONTRÁRIO FAZ BAIXA DO DIA ANTERIOR NORMALMENTE, PRIMEIRA VERIFICAÇÃO VALIDA SE JÁ HOUVE REGISTRO DO DADO
+    TABELA POR MEIO DO CAMPO NRDOCUMENTO. A SEGUNDA VALIDAÇÃO VERIFICA SE O REGISTRO É DO TIPO MONEYPLUS, SE SIM VERIFICA SE O CAMPO NOMECONTRAPARTE É NULO, SE SIM PREENCHE
+    O CAMPO
+    """
     data = int(date.today().strftime('%w'))
     if data == 1:
         sexta = (date.today() - timedelta(days=3)).strftime('%m-%d-%Y')
-        feriado = checa_dia(3)
+        feriado = checa_dia_extrato(3)
         if feriado:
-            pass 
+            return []
         else:
             lista_cobrancas = []
             try:
-                extrato_json = get_extrato(sexta)
-                lista_value = dict(extrato_json['value'])
-                for cobranca in lista_value['itens']:
-                    lista_cobrancas.append(json.dumps(cobranca))
-
+                print(sexta)
+                lista_value_sexta = get_extrato(sexta)
+                for cobranca in lista_value_sexta['itens']:
+                    qs = Item.objects.using('adm_int').filter(nrDocumento=cobranca['nrDocumento']).values()
+                    if qs:
+                        print(f"Documento já inserido na tabela: {cobranca['nrDocumento']}")
+                    else:
+                        if cobranca['natureza'] == 'C' and cobranca['cpfCnpj'] == 'cpf_client' and cobranca[
+                            'agenciaContraparte'] == '00000' and cobranca['contaContraparte'] == '0000000000':
+                            if not cobranca['nomeContraparte'] or cobranca['nomeContraparte'] == '' or cobranca[
+                                'nomeContraparte'] is None:
+                                moneyplus_sem_nomeContraparte = {
+                                    'natureza': cobranca['natureza'],
+                                    'dataLancto': cobranca['dataLancto'],
+                                    'nrDocumento': cobranca['nrDocumento'],
+                                    'cpfCnpj': cobranca['cpfCnpj'],
+                                    'nomeContraparte': 'MONEY PLUS SCM',
+                                    'agenciaContraparte': cobranca['agenciaContraparte'],
+                                    'contaContraparte': cobranca['contaContraparte'],
+                                    'valor': cobranca['valor'],
+                                    'saldoAtual': cobranca['saldoAtual'],
+                                    'saldoAnterior': cobranca['saldoAnterior'],
+                                    'tipoOperacao': cobranca['tipoOperacao'],
+                                    'codigo': cobranca['historico']['codigo'],
+                                    'descricao': cobranca['historico']['descricao'],
+                                    'complemento': cobranca['historico']['complemento'],
+                                    'categoria': cobranca['historico']['categoria'],
+                                }
+                                lista_cobrancas.append(moneyplus_sem_nomeContraparte)
+                            else:
+                                moneyplus_com_nomeContraparte = {
+                                    'natureza': cobranca['natureza'],
+                                    'dataLancto': cobranca['dataLancto'],
+                                    'nrDocumento': cobranca['nrDocumento'],
+                                    'cpfCnpj': cobranca['cpfCnpj'],
+                                    'nomeContraparte': cobranca['nomeContraparte'],
+                                    'agenciaContraparte': cobranca['agenciaContraparte'],
+                                    'contaContraparte': cobranca['contaContraparte'],
+                                    'valor': cobranca['valor'],
+                                    'saldoAtual': cobranca['saldoAtual'],
+                                    'saldoAnterior': cobranca['saldoAnterior'],
+                                    'tipoOperacao': cobranca['tipoOperacao'],
+                                    'codigo': cobranca['historico']['codigo'],
+                                    'descricao': cobranca['historico']['descricao'],
+                                    'complemento': cobranca['historico']['complemento'],
+                                    'categoria': cobranca['historico']['categoria'],
+                                }
+                                lista_cobrancas.append(moneyplus_com_nomeContraparte)
+                        else:
+                            dictio = {
+                                'natureza': cobranca['natureza'],
+                                'dataLancto': cobranca['dataLancto'],
+                                'nrDocumento': cobranca['nrDocumento'],
+                                'cpfCnpj': cobranca['cpfCnpj'],
+                                'nomeContraparte': cobranca['nomeContraparte'],
+                                'agenciaContraparte': cobranca['agenciaContraparte'],
+                                'contaContraparte': cobranca['contaContraparte'],
+                                'valor': cobranca['valor'],
+                                'saldoAtual': cobranca['saldoAtual'],
+                                'saldoAnterior': cobranca['saldoAnterior'],
+                                'tipoOperacao': cobranca['tipoOperacao'],
+                                'codigo': cobranca['historico']['codigo'],
+                                'descricao': cobranca['historico']['descricao'],
+                                'complemento': cobranca['historico']['complemento'],
+                                'categoria': cobranca['historico']['categoria'],
+                            }
+                            lista_cobrancas.append(dictio)
                 return lista_cobrancas
             except Exception as error:
                 print(error)
-    else:
+                return []
+       else:
         # ontem = (datetime.now() - timedelta(days=1)).replace(microsecond=0)
         ontem = (date.today() - timedelta(days=1)).strftime('%m-%d-%Y')
-        feriado = checa_dia(1)
+        feriado = checa_dia_extrato(1)
         if feriado:
-            pass 
+            return []
         else:
             lista_cobrancas = []
             try:
-                extrato_json = get_extrato(ontem)
-                lista_value = dict(extrato_json['value'])
+                lista_value = get_extrato(ontem)
                 for cobranca in lista_value['itens']:
-                    lista_cobrancas.append(json.dumps(cobranca))
-                
+                    qs = Item.objects.using('adm_int').filter(nrDocumento=cobranca['nrDocumento']).values()
+                    if qs:
+                        print(f"Documento já inserido na tabela: {cobranca['nrDocumento']}")
+                    else:
+                        if cobranca['natureza'] == 'C' and cobranca['cpfCnpj'] == 'cpf_client' and cobranca[
+                            'agenciaContraparte'] == '00000' and cobranca['contaContraparte'] == '0000000000':
+                            if not cobranca['nomeContraparte'] or cobranca['nomeContraparte'] == '' or cobranca[
+                                'nomeContraparte'] is None:
+                                moneyplus_sem_nomeContraparte = {
+                                    'natureza': cobranca['natureza'],
+                                    'dataLancto': cobranca['dataLancto'],
+                                    'nrDocumento': cobranca['nrDocumento'],
+                                    'cpfCnpj': cobranca['cpfCnpj'],
+                                    'nomeContraparte': 'MONEY PLUS SCM',
+                                    'agenciaContraparte': cobranca['agenciaContraparte'],
+                                    'contaContraparte': cobranca['contaContraparte'],
+                                    'valor': cobranca['valor'],
+                                    'saldoAtual': cobranca['saldoAtual'],
+                                    'saldoAnterior': cobranca['saldoAnterior'],
+                                    'tipoOperacao': cobranca['tipoOperacao'],
+                                    'codigo': cobranca['historico']['codigo'],
+                                    'descricao': cobranca['historico']['descricao'],
+                                    'complemento': cobranca['historico']['complemento'],
+                                    'categoria': cobranca['historico']['categoria'],
+                                }
+                                lista_cobrancas.append(moneyplus_sem_nomeContraparte)
+                            else:
+                                moneyplus_com_nomeContraparte = {
+                                    'natureza': cobranca['natureza'],
+                                    'dataLancto': cobranca['dataLancto'],
+                                    'nrDocumento': cobranca['nrDocumento'],
+                                    'cpfCnpj': cobranca['cpfCnpj'],
+                                    'nomeContraparte': cobranca['nomeContraparte'],
+                                    'agenciaContraparte': cobranca['agenciaContraparte'],
+                                    'contaContraparte': cobranca['contaContraparte'],
+                                    'valor': cobranca['valor'],
+                                    'saldoAtual': cobranca['saldoAtual'],
+                                    'saldoAnterior': cobranca['saldoAnterior'],
+                                    'tipoOperacao': cobranca['tipoOperacao'],
+                                    'codigo': cobranca['historico']['codigo'],
+                                    'descricao': cobranca['historico']['descricao'],
+                                    'complemento': cobranca['historico']['complemento'],
+                                    'categoria': cobranca['historico']['categoria'],
+                                }
+                                lista_cobrancas.append(moneyplus_com_nomeContraparte)
+                        else:
+                            dictio = {
+                                'natureza': cobranca['natureza'],
+                                'dataLancto': cobranca['dataLancto'],
+                                'nrDocumento': cobranca['nrDocumento'],
+                                'cpfCnpj': cobranca['cpfCnpj'],
+                                'nomeContraparte': cobranca['nomeContraparte'],
+                                'agenciaContraparte': cobranca['agenciaContraparte'],
+                                'contaContraparte': cobranca['contaContraparte'],
+                                'valor': cobranca['valor'],
+                                'saldoAtual': cobranca['saldoAtual'],
+                                'saldoAnterior': cobranca['saldoAnterior'],
+                                'tipoOperacao': cobranca['tipoOperacao'],
+                                'codigo': cobranca['historico']['codigo'],
+                                'descricao': cobranca['historico']['descricao'],
+                                'complemento': cobranca['historico']['complemento'],
+                                'categoria': cobranca['historico']['categoria'],
+                            }
+                            lista_cobrancas.append(dictio)
                 return lista_cobrancas
             except Exception as error:
                 print(error)
+                return []
